667ea6d4ea45886eca8fb54ff8751dad66d2bd93
emily <emily@bacheconsulting.com>
2023-06-18 15:50:11 +0200

    revert to starting position

--- python/tests/test_html_converter.py
+++ python/tests/test_html_converter.py
@@ -4,7 +4,6 @@ from html_converter import HtmlConverter
 
 
 def test_convert():
-    fake_file = io.StringIO("""foobar\n '' \n " """)
-    converter = HtmlConverter()
-    result = converter.convert_text_stream(fake_file)
-    assert result == "<html><p>foobar<p/><p> &#x27;&#x27;<p/><p> &quot;<p/></html>"
+    # TODO: write a test that fails due to the bug in
+    # HtmlConverter.convert_to_html
+    pass
