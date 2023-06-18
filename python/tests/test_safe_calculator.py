7de9d4185aebb428b2e4d8075015833dfd42a665
emily <emily@bacheconsulting.com>
2023-06-18 13:54:37 +0200

    improve the tests for calculator

---
+++ python/tests/test_safe_calculator.py
@@ -0,0 +1,42 @@
+from unittest.mock import Mock
+
+import pytest
+
+from safe_calculator import SafeCalculator, Authorizer
+
+
+class StubAuthorizer(Authorizer):
+    def __init__(self, stub_value):
+        self.stub_value = stub_value
+
+    def authorize(self):
+        return self.stub_value
+
+
+def test_divide_should_not_raise_any_error_when_authorized():
+    calculator = SafeCalculator(StubAuthorizer(True))
+    assert calculator.add(10, 2) == 12
+
+
+def test_divide_should_raise_error_when_not_authorized():
+    calculator = SafeCalculator(StubAuthorizer(False))
+    with pytest.raises(RuntimeError):
+        calculator.add(10, 2)
+
+
+def test_divide_should_not_raise_any_error_when_authorized_with_spy():
+    spy = Mock()
+    spy.authorize = Mock(return_value=True)
+    calculator = SafeCalculator(spy)
+    assert calculator.add(10, 2) == 12
+    spy.authorize.assert_called_once()
+
+
+def test_divide_should_raise_error_when_not_authorized_with_spy():
+    spy = Mock()
+    spy.authorize = Mock(return_value=False)
+    calculator = SafeCalculator(spy)
+    with pytest.raises(RuntimeError):
+        calculator.add(10, 2)
+    spy.authorize.assert_called_once()
+
