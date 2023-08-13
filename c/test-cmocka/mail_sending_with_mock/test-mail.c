469b15ed4e2662e50cbd41e8d4aad5331bf57fa1
Typist <typist@example.com>
2023-08-13 14:04:20 +0000

    test for mail sending that fails with spy

--- c/test-cmocka/mail_sending_with_mock/test-mail.c
+++ c/test-cmocka/mail_sending_with_mock/test-mail.c
@@ -29,7 +29,39 @@ int check_request(const LargestIntegralType actual_value, const LargestIntegralT
     return 0;
 }
 
-static void test_send_one_user(void **state)
+static void test_send_one_user_with_mock(void **state)
+{
+    User* user;
+    Request* expected_request;
+    Response* response;
+    bool success;
+    int response_code = 500;
+    char name[MAX_NAME_LENGTH];
+    char email[MAX_NAME_LENGTH];
+    char subject[MAX_NAME_LENGTH];
+    char message[MAX_MESSAGE_LENGTH];
+
+    (void)state;  // unused variable
+
+    will_return(__wrap_http_client_post, true);
+
+    strncpy(name, "name", MAX_NAME_LENGTH -1);
+    strncpy(email, "email", MAX_NAME_LENGTH -1);
+    strncpy(subject, "New notification", MAX_NAME_LENGTH -1);
+    strncpy(message, "message", MAX_MESSAGE_LENGTH -1);
+
+    user = User_create(name, email);
+    response = Response_create(response_code);
+    expected_request = Request_create(name, email, subject, message);
+
+    expect_check(__wrap_http_client_post, request, check_request, expected_request);
+
+    success = send_mail(user, message, response);
+    assert_true(success);
+    assert_int_equal(200, response->code);
+}
+
+static void test_send_one_user_with_spy(void **state)
 {
     User* user;
     Request* expected_request;
@@ -65,7 +97,7 @@ static void test_send_one_user(void **state)
 
 int main(void) {
     const struct CMUnitTest tests[] = {
-            cmocka_unit_test(test_send_one_user),
+            cmocka_unit_test(test_send_one_user_with_mock),
             };
 
     return cmocka_run_group_tests(tests, NULL, NULL);
---
+++ c/test-cmocka/mail_sending_with_spy/test-mail-spy.c
@@ -0,0 +1,58 @@
+#include <stdarg.h>
+#include <setjmp.h>
+#include <stddef.h>
+#include <stdbool.h>
+#include "cmocka.h"
+#include <stdlib.h>
+#include <string.h>
+#include "mail_sending.h"
+
+Request *actual_request;
+
+bool __wrap_http_client_post(Request* request, Response* response);
+bool __wrap_http_client_post(Request* request, Response* response) {
+    actual_request = request;
+    response->code = 200;
+    return mock_type(bool);
+}
+
+static void test_send_one_user_with_spy(void **state)
+{
+    User* user;
+    Response* response;
+    int response_code = 500;
+    char name[MAX_NAME_LENGTH];
+    char email[MAX_NAME_LENGTH];
+    char subject[MAX_NAME_LENGTH];
+    char message[MAX_MESSAGE_LENGTH];
+
+    (void)state;  // unused variable
+
+    will_return(__wrap_http_client_post, true);
+
+    strncpy(name, "name", MAX_NAME_LENGTH -1);
+    strncpy(email, "email", MAX_NAME_LENGTH -1);
+    strncpy(subject, "New notification", MAX_NAME_LENGTH -1);
+    strncpy(message, "message", MAX_MESSAGE_LENGTH -1);
+
+    user = User_create(name, email);
+    response = Response_create(response_code);
+
+    send_mail(user, message, response);
+
+    assert_string_equal(actual_request->name, name);
+    assert_string_equal(actual_request->email, email);
+    assert_string_equal(actual_request->subject, subject);
+    assert_string_equal(actual_request->message, message);
+}
+
+
+
+
+int main(void) {
+    const struct CMUnitTest tests[] = {
+            cmocka_unit_test(test_send_one_user_with_spy),
+            };
+
+    return cmocka_run_group_tests(tests, NULL, NULL);
+}
