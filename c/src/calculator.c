41cc92d79f710a3d56b53ddbd0a6702913aada7e
emily <emily@bacheconsulting.com>
2023-08-14 08:17:23 +0200

    put in the bug in calculator

--- c/src/calculator.c
+++ c/src/calculator.c
@@ -3,7 +3,8 @@
 
 bool calculator_add(int left, int right, int *result)
 {
-    if (calculation_is_authorized()) {
+    // BUG! Should be calculation_is_authorized() without the !
+    if (!calculation_is_authorized()) {
         *result = left + right;
         return true;
     } else {
