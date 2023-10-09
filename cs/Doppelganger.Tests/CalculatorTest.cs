463780c7b1289e5701ce98e86f5186093c6e377e
emily <emily@bacheconsulting.com>
2023-10-09 14:55:35 +0200

    Sample solution for CalculatorTest

--- cs/Doppelganger.Tests/CalculatorTest.cs
+++ cs/Doppelganger.Tests/CalculatorTest.cs
@@ -1,4 +1,5 @@
 using Doppelganger;
+using Moq;
 
 namespace Doppelganger.Tests
 {
@@ -7,7 +8,50 @@ namespace Doppelganger.Tests
         [Test]
         public void DoNotThrowWhenAuthorized()
         {
-            // TODO: trigger the bug in Calculator.Divide() by implementing the Authorizer interface
+            var authorizer = new StubAuthorizer(true);
+            var calculator = new Calculator(authorizer);
+            Assert.AreEqual(3, calculator.Add(1,2));
+        }        
+        
+        [Test]
+        public void ThrowWhenNotAuthorized()
+        {
+            var authorizer = new StubAuthorizer(false);
+            var calculator = new Calculator(authorizer);
+            Assert.Throws<Exception>(() => calculator.Add(1, 2));
+        }
+        
+        [Test]
+        public void DoNotThrowWhenAuthorized_Moq()
+        {
+            var authorizer = new Mock<IAuthorizer>();
+            authorizer.Setup(a => a.Authorize()).Returns(true);
+            var calculator = new Calculator(authorizer.Object);
+            Assert.AreEqual(3, calculator.Add(1,2));
+        }
+        
+        [Test]
+        public void ThrowWhenNotAuthorized_Moq()
+        {
+            var authorizer = new Mock<IAuthorizer>();
+            authorizer.Setup(a => a.Authorize()).Returns(false);
+            var calculator = new Calculator(authorizer.Object);
+            Assert.Throws<Exception>(() => calculator.Add(1, 2));
+        }
+    }
+
+    public class StubAuthorizer : IAuthorizer
+    {
+        private bool _authorize;
+
+        public StubAuthorizer(bool authorize)
+        {
+            _authorize = authorize;
+        }
+
+        public bool Authorize()
+        {
+            return _authorize;
         }
     }
 }
